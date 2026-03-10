166f1336bca8e2a59b53da7218df612de67c3c5d
sahijwaraich13 <sahijwaraich05@gmail.com>
2026-03-09 17:02:59 -0700

    Delete data/scripts directory

--- data/scripts/data_loader.py
+++
@@ -1,4 +0,0 @@
-import kagglehub
-# This downloads the 6GB to a cache folder on your Mac automatically
-path = kagglehub.dataset_download("frankcaoyun/stocktwits-2020-2022-raw")
-print(f"Dataset downloaded to: {path}")
