426416d87c3f1c0a266996e1a2750146d9d6ea3e
WilsoAgya <wilsonagyapong16@gmail.com>
2026-03-24 14:44:49 -0700

    merged closing prices of cleaned sentiment data with yfinance api data

--- data/groundtruth.py
+++ data/groundtruth.py
@@ -2,6 +2,9 @@ import yfinance as yf
 import kagglehub
 
 
-search_results = yf.Search("apple")
-print(search_results)
+#search_results = yf.Search("apple")
+#print(search_results)
+
+ticker = yf.Ticker("AAPL")
+print(ticker.recommendations)
 
