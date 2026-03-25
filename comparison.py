0305c6c7eb56de2e4bbf7b75ba18b330c8879729
Balkarsingh1 <balkarsingh1@student.ufv.ca>
2026-03-25 16:20:14 -0700

    Loading in the baseline raw data and final labeled data static csv files

--- comparison.py
+++ comparison.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+# loading files for comparison
+merged_df = pd.read_csv('AAPL_Merged_Sentiment_Stock.csv')
+final_cleaned_df = pd.read_csv('apple_fully_labeled_sentiment.csv')
+
+print(f"Merged df: {len(merged_df)} rows")
+print(f"Final cleaned df: {len(final_cleaned_df)} rows")
 def check_accuracy(row):
     if row['majority_sentiment'] == 'Bullish' and row['price_change'] > 0:
         return 1
@@ -7,4 +13,7 @@ def check_accuracy(row):
         return 1
     else:
         return 0
+    
 
+# Raw baseline data categorizating
+raw_df = merged_df[merged_df['sentiment_clean'].isin(['Bullish', 'Bearish'])].copy()
